68b9f82ac230ceb99caa4bfc0118af66f414e22c
llewellynmeldrum <llewellyn.meldrum1@students.mq.edu.au>
2025-08-27 18:40:41 +1000

    Updated error functions to better reflect the book

--- clox/include/clox_common.h
+++ clox/include/clox_common.h
@@ -1,22 +1,23 @@
 #ifndef CLOX_COMMON_H
 #define CLOX_COMMON_H
 
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+
+
 #define PRINTF_FATAL_ERR(fmt, ...)do{\
 	fprintf(stderr,fmt, ##__VA_ARGS__);\
 	fprintf(stderr,"\nexiting...\n");\
 	exit(EXIT_FAILURE);\
 	}while(0)
 
-#define PRINTF_ERR_LN(fmt, ...)do{\
-	fprintf(stderr,"Error on line %d:\n", scanner.line);\
+#define error(fmt, ...)do{\
+	fprintf(stderr,"[line %d] Error:\n", scanner.line);\
 	fprintf(stderr,fmt "\n", ##__VA_ARGS__);\
 	}while(0);
 
 #define MAX_LINE_LEN	1024
-
-#include <stdbool.h>
-#include <stddef.h>
-#include <stdint.h>
 
 // Prints invisible chars like \n, \c as string literals
 char *printVerboseChar(const char c);
